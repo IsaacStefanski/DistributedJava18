ead474b3b8de9cdb48e5f411101a0d3252c45a71
IsaacStefanski <icstefans2@gmail.com>
2018-02-27 20:27:42 -0600

    JSF

---
+++ src/main/java/edu/wctc/dj/week6/model/NameService.java
@@ -0,0 +1,43 @@
+package edu.wctc.dj.week6.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author Isaac
+ */
+public class NameService {
+    
+    private List<Name> nameList = Arrays.asList(
+        new Name("1", "Fred", "Jones", new Address("123 Main Street", "Waukesha", "WI", "53186")),
+        new Name("2", "Joe", "Smith", new Address("456 1st Street", "Milwaukee", "WI", "53172"))
+    );
+    
+    public Name getName(String id){
+        Name name = null;
+        for (Name n : nameList){
+            if (n.getId().equals(id)){
+                name = n;
+            }
+        }
+        return name;
+    }
+    
+    public List<Name> getAllNames(){
+        return nameList;
+    }
+    
+    public List<Name> findNames(String search){
+        search = search.toLowerCase();
+        List<Name> list = new ArrayList<Name>();
+        for (Name name : nameList){
+            if(name.getFirst().toLowerCase().startsWith(search) ||
+                    name.getLast().toLowerCase().startsWith(search)){
+                list.add(name);
+            }
+        }
+        return list;
+    }
+}
