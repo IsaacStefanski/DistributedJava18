cd5f381d6afbcb4cdaac8d4e538e38096ae506d0
IsaacStefanski <icstefans2@gmail.com>
2018-03-13 20:06:06 -0500

    Cart, Cart Service, Cart Bean

---
+++ src/main/java/edu/wctc/dj/week6/model/ShoppingCartService.java
@@ -0,0 +1,25 @@
+package edu.wctc.dj.week6.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Isaac
+ */
+public class ShoppingCartService {
+    private static final Map<String, ShoppingCart> contents = new HashMap<>();
+    
+    public ShoppingCart getContents(String sessionId){
+        ShoppingCart cart = contents.computeIfAbsent(sessionId, (String s) -> new ShoppingCart());
+        return cart;
+    }
+    
+    public void update(String sessionId, ShoppingCart cart){
+        contents.put(sessionId, cart);
+    }
+    
+    public void delete(String sessionId){
+        contents.remove(sessionId);
+    }
+}
