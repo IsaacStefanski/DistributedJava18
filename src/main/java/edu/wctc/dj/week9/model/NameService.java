1f944c81ee567a0d66663fe6849f30300b25ba21
IsaacStefanski <icstefans2@gmail.com>
2018-03-27 19:58:46 -0500

    Use DAO pattern

--- src/main/java/edu/wctc/dj/week9/model/NameService.java
+++ src/main/java/edu/wctc/dj/week9/model/NameService.java
@@ -1,43 +1,32 @@
 package edu.wctc.dj.week9.model;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import edu.wctc.dj.week9.data.AddressDAO;
+import edu.wctc.dj.week9.data.NameDAO;
 import java.util.List;
 
 /**
  *
  * @author Isaac
  */
-public class NameService {
-    
-    private List<Name> nameList = Arrays.asList(
-        new Name("1", "Fred", "Jones", new Address("123 Main Street", "Waukesha", "WI", "53186")),
-        new Name("2", "Joe", "Smith", new Address("456 1st Street", "Milwaukee", "WI", "53172"))
-    );
-    
+public class NameService {   
     public Name getName(String id){
-        Name name = null;
-        for (Name n : nameList){
-            if (n.getId().equals(id)){
-                name = n;
-            }
-        }
-        return name;
+        return null;
     }
     
-    public List<Name> getAllNames(){
+    public List<Name> getAllNames() throws Exception{
+        NameDAO nameDAO = new NameDAO();
+        List<Name> nameList = nameDAO.getNames();
+        
+        if(nameList != null){
+            AddressDAO addressDAO = new AddressDAO();
+            for(Name n : nameList){
+                n.setAddress(addressDAO.getAddress(n));
+            }
+        }
         return nameList;
     }
     
     public List<Name> findNames(String search){
-        search = search.toLowerCase();
-        List<Name> list = new ArrayList<Name>();
-        for (Name name : nameList){
-            if(name.getFirst().toLowerCase().startsWith(search) ||
-                    name.getLast().toLowerCase().startsWith(search)){
-                list.add(name);
-            }
-        }
-        return list;
+        return null;
     }
 }
