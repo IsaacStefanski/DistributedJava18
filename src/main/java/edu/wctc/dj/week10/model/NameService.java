b8fa38b82294c32ffbc816f3690b089d6cc34e60
IsaacStefanski <icstefans2@gmail.com>
2018-04-03 18:29:26 -0500

    Names App Week 10

---
+++ src/main/java/edu/wctc/dj/week10/model/NameService.java
@@ -0,0 +1,32 @@
+package edu.wctc.dj.week10.model;
+
+import edu.wctc.dj.week10.data.AddressDAO;
+import edu.wctc.dj.week10.data.NameDAO;
+import java.util.List;
+
+/**
+ *
+ * @author Isaac
+ */
+public class NameService {   
+    public Name getName(String id){
+        return null;
+    }
+    
+    public List<Name> getAllNames() throws Exception{
+        NameDAO nameDAO = new NameDAO();
+        List<Name> nameList = nameDAO.getNames();
+        
+        if(nameList != null){
+            AddressDAO addressDAO = new AddressDAO();
+            for(Name n : nameList){
+                n.setAddress(addressDAO.getAddress(n));
+            }
+        }
+        return nameList;
+    }
+    
+    public List<Name> findNames(String search){
+        return null;
+    }
+}
