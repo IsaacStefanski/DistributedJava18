ab0b2ed22d6b0ad34c0c107230a09ae54dc53877
IsaacStefanski <icstefans2@gmail.com>
2018-04-17 19:10:54 -0500

    Copy files, create META-INF and WelcomePage

---
+++ src/main/java/edu/wctc/dj/week12/namesapp12/NamesApp.java
@@ -0,0 +1,15 @@
+package edu.wctc.dj.week12.namesapp12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Isaac
+ */
+@SpringBootApplication
+public class NamesApp {
+    public static void main(String[] args) {
+        SpringApplication.run(NamesApp.class, args);
+    }
+}
