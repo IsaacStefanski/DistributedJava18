3037cddad1596316caa096c90a1e5ee837dc56f7
IsaacStefanski <icstefans2@gmail.com>
2018-04-24 18:39:04 -0500

    NamesApp13 Week 13 REST

---
+++ src/main/java/edu/wctc/dj/week13/namesapp13/data/dao/INameDAO.java
@@ -0,0 +1,12 @@
+package edu.wctc.dj.week13.namesapp13.data.dao;
+
+import edu.wctc.dj.week13.namesapp13.model.Name;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author Isaac
+ */
+public interface INameDAO extends JpaRepository<Name, String> {
+    
+}
